b166a687850472f1d5868b801295c5446c61ef11
Kenneth <kennethaboy2k16>
2017-07-11 14:57:24 +0800

    MODIFIED

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,78 +5,83 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="js/bootstrap.min.js">
 	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/style.css">
 <body>
-<div class = "container">
-	<div class = "col-lg-12 col-md-12 column-header">
-			<h1 class = "module">Personal Data Sheet</h1>
-			<p class = "small-text"></p>
-		<div class = "col-sm-4 pull-right">
-			<p><label>CS ID No.</label>	<input type = "text" class = "form-control"/></p>
-		</div>
-	</div>
-	<div class = "col-lg-12 column-container">
-	<span style = "font-style:italic;">I. Personal Information</span>
-	</div>
-	<div class = "col-lg-12 col-md-12 col-sm-12 column-body">
-		<div class = "col-lg-3 col-md-3 col-sm-3 grayscale">
-			<p class = "title">SURNAME</p>
-			<p class = "title">FIRST NAME</p>
-			<p class = "title">MIDDLE NAME</p>
-			<p class = "title">NAME EXTENSION</p>
-		</div>	
-		<div class = "col-lg-9 col-md-9 col-sm- whitescale">
-		<form method = "POST">
-			<input type = "text" class = "form-control no-border" name = "surname" >	
-			<input type = "text" class = "form-control no-border" name = "firstname">	
-			<input type = "text" class = "form-control no-border" name = "middlename">
-			<input type = "text" class = "form-control no-border" name = "name_extension" placeholder="example SR. JR. etc..">
-		</div>
-	<div class = "col-lg-12 column-body">
-			<div class = "col-lg-5 col-xs-5 col-md-5 birthday">
-				<div class = "col-lg-4 col-md-4 col-sm-4 grayscale2">
-			<p class = "title">DATE OF BIRTH</p>
-			<p class = "title">PLACE OF BIRTH</p>
-			<p class = "title">SEX</p>			
-		</div>	
-		<div class = "col-lg-8 col-md-8 col-sm-8 whitescale">
-			<input type = "date" id = "datepicker" class = "form-control no-border2" name = "date_of_birth" >	
-			<input type = "text" class = "form-control no-border2" name = "place_of_birth">	
-			<div class="radio-inline break">
- 				 <label>
-   					 <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
- 						MALE
- 				 </label>
-			</div>
-			<div class="radio-inline break">
-  				<label>
-   					 <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-  						FEMALE
-  				</label>
-			</div>	
-		</div>
-			</div>
-
-			<div class = "col-lg-7 col-xs-7 col-md-7" style = "padding:0px;">
-				<div class = "col-lg-4 col-md-4 col-sm-4 citizenship" style = "">
-					<p class = "texture">CITIZENSHIP <br/><span class="tiny">If holder of dual citizenship,<br/> please indicate the details.</span></p>				
-				</div>	
-				<div class = "col-lg-8 col-md-8 cit-right">
-					<div class="radio-inline break">
-		 				 <label>
-		   					 <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-		 						Filipino
-		 				 </label>
-					</div>
-					<div class="radio-inline break">
-		  				<label>
-		   					 <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-		  						Dual Citizenship
-		  				</label>
-					</div>	
-				</div>	
-			</div>
-	</form>
-	</div>
+	<table class = "table table-bordered table-responsive">
+		<tbody>
+			<tr style = "padding:0px !important;">
+				<td>
+					<h3 style = "text-align:center;">Personal Data Sheet</h3>				
+					<p class = "small-text">WARNING: Any misrepresentation made in the Personal Data Sheet and the Work Experience Sheet shall cause the filing of administrative/criminal case/s against the person concerned.</p>
+					<p class = "tiny-text">READ THE ATTACHED GUIDE TO FILLING OUT THE PERSONAL DATA SHEET (PDS) BEFORE ACCOMPLISHING THE PDS FORM.</p>
+					<p class = "tiny-text">Print legibly. Tick appropriate boxes (     ) and use separate sheet if necessary. Indicate N/A if not applicable.  DO NOT ABBREVIATE <span class = "pull-right"><input type = "text" class= "csid" ></span> </p>
+				</td>				
+			</tr>
+			<tr>
+				<td>I. Personal Information</td>
+			</tr>			
+		</tbody>		
+	</table>
+<div class = "container-fluid">
+<div class="row" style = "border:1px solid">
+  <div class="col-xs-4 col-md-4 bord">SURNAME</div>
+  <div class="col-xs-8 col-sm-8 col-md-8 bord" style = "border:1px solid">Tubera</div>
+  <div class="col-xs-4 col-md-4 bord" style = "border:1px solid">FIRSTNAME</div>
+  <div class="col-xs-8 col-sm-8 col-md-8 bord" style = "border:1px solid">Chris</div>
+  <div class="col-xs-4 col-md-4 bord" style = "border:1px solid">MIDDLENAME</div>
+  <div class="col-xs-5 col-sm-5 col-md-5 bord" style = "border:1px solid">Laurence</div>
+  <div class="col-xs-3 col-sm-3 col-md-3 bord" style = "border:1px solid"><span style = "font-size:6px">NAME EXTENSION </span></div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Date of Birth</span></div>
+  <div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 bord">August 25, 1991</div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Citizenship </span></div>
+  <div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 bord" style = "">Filipino, American </div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Place of Birth</span></div>
+  <div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 bord">Zamboanga City</div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Sex</span></div>
+  <div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 bord">Male</div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Civil Status</span></div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord">Single</div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Height</span></div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord">167 cm.</div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Weight</span></div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord">50 kilo's</div>
+  <div class = "col-lg-12 col-md-12 col-xs-12 col-sm-12  bord" disabled>Complete Home Address</div>
+  
+  <div class = "col-lg-4 col-md-4 col-xs-4 col-sm-4 bord">Street : </div>
+  <div class = "col-lg-8 col-md-8 col-xs-8 col-sm-8 bord"> Lot 3 Rizal Street </div>
+  <div class = "col-lg-4 col-md-4 col-xs-4 col-sm-4 bord">Barangay : </div>
+  <div class = "col-lg-8 col-md-8 col-xs-8 col-sm-8 bord"> Guinhalaran </div>
+  <div class = "col-lg-4 col-md-4 col-xs-4 col-sm-4 bord">City : </div>
+  <div class = "col-lg-8 col-md-8 col-xs-8 col-sm-8 bord">  Silay City</div>
+  <div class = "col-lg-4 col-md-4 col-xs-4 col-sm-4 bord">Province: </div>
+  <div class = "col-lg-4 col-md-4 col-xs-4 col-sm-4 bord">Negros Occidental</div>
+  <div class = "col-lg-2 col-md-2 col-xs-2 col-sm-2 bord">Zip Code </div>
+  <div class = "col-lg-2 col-md-2 col-xs-2 col-sm-2 bord"> 6116 </div>
+   <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord"><span>Blood Type</span></div>
+  <div class = "col-xs-2 col-sm-2 col-md-2 col-lg-2 bord">A+</div>
+   <div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 bord"><span>GSIS No.</span></div>
+	<div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4 bord">9841-1241-1212</div>
+    <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>PAG-IBIG ID NO.</span></div>
+  <div class = "col-xs-3 col-sm-63col-md-3 col-lg-3 bord">09841-1241-1212</div>
+     <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>PhilHealth No.</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">41-1241-1212</div>
+   <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>SSS No.</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">41-1241-1212</div>
+   <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>TIN No.</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">0981-1241-1212</div>
+   <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>Agency Employee No.</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">09-1241-1212</div>
+    <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>Email Address</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">ltubera@gmail.com</div>
+    <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>Phone Number</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">&nbsp;</div>
+   <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord"><span>Mobile Number</span></div>
+  <div class = "col-xs-3 col-sm-3 col-md-3 col-lg-3 bord">&nbsp;</div>
+  
+  
 </div>
+
+</div>
+
 </body>
 </html>
